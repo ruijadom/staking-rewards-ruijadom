a239a334b62fac40064f2c5c57a51269d10f18ba
ruijadom <ruijadomingues@gmail.com>
2023-07-21 00:31:16 +0100

    chore(prisma): init and config tailwindcss

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,11 +1,13 @@
 import { trpc } from "./utils/trpc";
 
+import { cn } from "@ruijadom/utils" 
+
 function App() {
   const { isError, isLoading, isSuccess, data, error } =
     trpc.getStakings.useQuery();
 
   return (
-    <div className="App">
+    <div className={cn("bg-primary-500")}>
       <h1>Staking Rewards</h1>
       {isLoading ? (
         <p>loading</p>
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,7 +3,9 @@ import { httpBatchLink } from "@trpc/client";
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+
+import "./styles/globals.css";
+
 import { trpc } from "./utils/trpc";
 
 function MyApp() {
